25666c41f63cf486b38954898346cf280b7546e0
IvanOmelchenkoIP <io25112002@gmail.com>
2022-09-27 23:49:33 +0300

    Add methods implementations
    Add implementations for generate() and getMostZeroesBinary() methods

--- java/src/lab1/PrimeArray.java
+++ java/src/lab1/PrimeArray.java
@@ -10,13 +10,38 @@ public class PrimeArray {
 	PrimeArray(int n) {
 		numbers = generate(n);
 	}
-	
-	private ArrayList<Integer> generate (int n) {		
+	 
+	private ArrayList<Integer> generate (int n) {
+		//Eratosthenes Sieve algorithm for generating prime numbers
+		boolean[] primes = new boolean[n + 1];
+		Arrays.fill(primes, true);
+		for (int i = 2; i * i <= n; i++) {
+			if (primes[i]) {
+				for (int j = i * i; j <= n; j += i) primes[j] = false;
+			}
+		}
+			
 		ArrayList<Integer> numberList = new ArrayList<Integer>();
+		for (int i = 2; i <= n; i++) {
+			if (primes[i]) numberList.add(i);
+		}
+			
 		return numberList;
 	}
 	
 	public int getMostZeroesBinary() {
-		return 0;
+		if (numbers.size() == 0) return 0; 
+		
+		int mostZeroes = 0;
+		int largest = 0;
+		for (int number : numbers) {
+			String binary = Integer.toBinaryString(number);
+			int zeroes = binary.replaceAll("1", "").length();
+			if (zeroes > mostZeroes) { 
+				mostZeroes = zeroes;
+				largest = number; 
+			}
+		}
+		return largest;
 	}
 }
