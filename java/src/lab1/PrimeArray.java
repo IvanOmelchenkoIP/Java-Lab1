54de097c47178734ff9e6ee11979b593258ea57d
IvanOmelchenkoIP <io25112002@gmail.com>
2022-09-27 22:48:56 +0300

    Add class for solving task
    Add PrimeArray class which contains methods will solve the task

---
+++ java/src/lab1/PrimeArray.java
@@ -0,0 +1,22 @@
+package lab1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PrimeArray {
+	
+	private ArrayList<Integer> numbers;
+	
+	PrimeArray(int n) {
+		numbers = generate(n);
+	}
+	
+	private ArrayList<Integer> generate (int n) {		
+		ArrayList<Integer> numberList = new ArrayList<Integer>();
+		return numberList;
+	}
+	
+	public int getMostZeroesBinary() {
+		return 0;
+	}
+}
